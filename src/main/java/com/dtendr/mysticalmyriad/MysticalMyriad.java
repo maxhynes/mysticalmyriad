836e1f5857e8c822c3cd7d8bf22371b0f27bd0ac
Joshua Davis <dtendr@gmail.com>
2015-02-21 07:40:21 -0500

    Build script and base class created/edited

---
+++ src/main/java/com/dtendr/mysticalmyriad/MysticalMyriad.java
@@ -0,0 +1,28 @@
+package com.dtendr.mysticalmyriad;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="MysticalMyriad", name="Mystical Myriad", version="1.7.10-1.0")
+public class MysticalMyriad
+{
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
